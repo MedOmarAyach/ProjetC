e2067eb2c2dc51765dea1bd0050ea985472f4c93
MedOmarAyach <154985717+MedOmarAyach@users.noreply.github.com>
2023-12-27 13:40:42 +0100

    Add files via upload

---
+++ projet.cpp
@@ -0,0 +1,161 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_CLIENTS 100
+//strucutre pour represter un client
+struct Client {
+    int numero_compte;
+    char nom[50];
+    float solde;
+};
+//tableau pour enregistrer les clients
+Client clients[MAX_CLIENTS];
+int totalClients = 0;
+//fonctin pour creer un compte
+void creerCompte() {
+    if (totalClients < MAX_CLIENTS) {
+        printf("Entrez le numéro de compte : ");
+        scanf("%d", &clients[totalClients].numero_compte);
+        printf("Entrez le nom du client : ");
+        scanf("%s", clients[totalClients].nom);
+        clients[totalClients].solde = 0.0;
+        totalClients++;
+        printf("Compte créé avec succès .\n");
+    } else {
+        printf("Nombre maximum de clients atteint .\n");
+    }
+}
+//mise a jour du compte
+void mettreAJourCompte() {
+    int numeroCompte;
+    printf("Entrez le numéro de compte à mettre à jour : ");
+    scanf("%d", &numeroCompte);
+    int index = -1;
+    for (int i = 0; i < totalClients; i++) {
+        if (clients[i].numero_compte == numeroCompte) {
+            index = i;
+            break;
+        }
+    }
+    if (index != -1) {
+        printf("Entrez le nouveau solde : ");
+        scanf("%f", &clients[index].solde);
+        printf("Compte mis à jour avec succès .\n");
+    } else {
+        printf("Compte non trouvé .\n");
+    }
+}
+//affichage d'un seul compte
+void afficherDetailsCompte() {
+    int numeroCompte;
+    printf("Entrez le numéro de compte pour afficher les détails : ");
+    scanf("%d", &numeroCompte);
+    int index = -1;
+    for (int i = 0; i < totalClients; i++) {
+        if (clients[i].numero_compte == numeroCompte) {
+            index = i;
+            break;
+        }
+    }
+    if (index != -1) {
+        printf("Numéro de compte : %d\n", clients[index].numero_compte);
+        printf("Nom du client : %s\n", clients[index].nom);
+        printf("Solde : %.2f\n", clients[index].solde);
+    } else {
+        printf("Compte non trouvé .\n");
+    }
+}
+///affichage du liste des compte
+void afficherTousLesClients() {
+    if (totalClients > 0) {
+        printf("Liste des clients :\n");
+        for (int i = 0; i < totalClients; i++) {
+            printf("%d. %s\n", clients[i].numero_compte, clients[i].nom);
+        }
+    } else {
+        printf("Aucun client disponible .\n");
+    }
+}
+//nouveau tache: virement d'un compte vers un autre
+void effectuerTransaction() {
+    int numeroCompteSrc, numeroCompteDest;
+    float montant;
+    printf("Entrez le numéro de compte source : ");
+    scanf("%d", &numeroCompteSrc);
+    int indexSrc = -1;
+    for (int i = 0; i < totalClients; i++) {
+        if (clients[i].numero_compte == numeroCompteSrc) {
+            indexSrc = i;
+            break;
+        }
+    }
+    if (indexSrc != -1) {
+        printf("Entrez le numéro de compte de destination : ");
+        scanf("%d", &numeroCompteDest);
+
+        int indexDest = -1;
+        for (int i = 0; i < totalClients; i++) {
+            if (clients[i].numero_compte == numeroCompteDest) {
+                indexDest = i;
+                break;
+            }
+        }
+        if (indexDest != -1) {
+            printf("Entrez le montant à transférer : ");
+            scanf("%f", &montant);
+
+            if (montant <= clients[indexSrc].solde) {
+                clients[indexSrc].solde -= montant;
+                clients[indexDest].solde += montant;
+                printf("Transfert réussi , Nouveau solde du compte source (%d) : %.2f\n", numeroCompteSrc, clients[indexSrc].solde);
+                printf("Nouveau solde du compte de destination (%d) : %.2f\n", numeroCompteDest, clients[indexDest].solde);
+            } else {
+                printf("Fonds insuffisants dans le compte source .\n");
+            }
+        } else {
+            printf("Compte de destination non trouvé .\n");
+        }
+    } else {
+        printf("Compte source non trouvé .\n");
+    }
+}
+int main() {
+    int choix;
+    do {
+        printf("\nMenu E-Bank :\n");
+        printf("1. Créer un nouveau compte\n");
+        printf("2. Mettre à jour un compte\n");
+        printf("3. Afficher les détails d'un compte\n");
+        printf("4. Afficher tous les clients\n");
+        printf("5. Effectuer une transaction\n");
+        printf("6. Quitter\n");
+        printf("Entrez votre choix : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1:
+                creerCompte();
+                break;
+            case 2:
+                mettreAJourCompte();
+                break;
+            case 3:
+                afficherDetailsCompte();
+                break;
+            case 4:
+                afficherTousLesClients();
+                break;
+            case 5:
+                effectuerTransaction();
+                break;
+            case 6:
+                printf("Fermeture d'E-Bank. Au revoir .\n");
+                break;
+            default:
+                printf("Choix invalide , Veuillez réessayer.\n");
+        }
+    } while (choix != 6);
+    return 0;
+}
+
